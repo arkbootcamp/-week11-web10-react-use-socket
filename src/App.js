526c50154f356a0071dd17b5318fe1677531b7a3
risano <muhammadrisano@gmail.com>
2021-08-27 11:47:29 +0700

    initial socket

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import io from 'socket.io-client'
+import {useEffect} from 'react'
 function App() {
+  useEffect(() => {
+    const socket = io('http://localhost:4000')
+    console.log(socket);
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Aplikasi react socket</h1>
     </div>
   );
 }
